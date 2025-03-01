b26ccec35adc991227b18387bd314d414df3cb94
tomenglish23 <tomenglish23@gmail.com>
2025-03-01 15:02:00 -0500

    Updates

---
+++ precache-manifest.28e8294f3f95637edde7dcf61ee1b46a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7634c5223d14fd73ceae3f8cbdb0955e",
+    "url": "/TomEnglishAI/index.html"
+  },
+  {
+    "revision": "afc8e4a2f10ea8f174ea",
+    "url": "/TomEnglishAI/static/css/main.b1df637c.chunk.css"
+  },
+  {
+    "revision": "e10dbb37bbe27e6baba1",
+    "url": "/TomEnglishAI/static/js/2.907f8bca.chunk.js"
+  },
+  {
+    "revision": "9e126fb458b379ef7cf758f298725956",
+    "url": "/TomEnglishAI/static/js/2.907f8bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936b77c21ffdb9a6225e",
+    "url": "/TomEnglishAI/static/js/3.07ec4502.chunk.js"
+  },
+  {
+    "revision": "afc8e4a2f10ea8f174ea",
+    "url": "/TomEnglishAI/static/js/main.0aa521e4.chunk.js"
+  },
+  {
+    "revision": "ef306f7fc0e71e5ce4b8",
+    "url": "/TomEnglishAI/static/js/runtime-main.f6e5a3b5.js"
+  },
+  {
+    "revision": "d933811650a919e6f2fa9dbfe9ee0938",
+    "url": "/TomEnglishAI/static/media/AIInFinance1.d9338116.jpg"
+  },
+  {
+    "revision": "45d94fb5ff9accc5320afbadd6b1b835",
+    "url": "/TomEnglishAI/static/media/DogTrack1.45d94fb5.jpg"
+  },
+  {
+    "revision": "d28924257d0b21f775a1a96366fb1d4a",
+    "url": "/TomEnglishAI/static/media/DogTrack2.d2892425.png"
+  },
+  {
+    "revision": "1d31553e14e4ac044475f7aad4647ca8",
+    "url": "/TomEnglishAI/static/media/FullStack2.1d31553e.jpg"
+  },
+  {
+    "revision": "cc250fe322f79b7c3fd78e069586c68e",
+    "url": "/TomEnglishAI/static/media/MLExample1.cc250fe3.jpg"
+  },
+  {
+    "revision": "2f206b3d39772536d20d0d1a0b5206c8",
+    "url": "/TomEnglishAI/static/media/OldManInDingy.2f206b3d.jpg"
+  },
+  {
+    "revision": "83756dd637124481bb71db92afe9af7e",
+    "url": "/TomEnglishAI/static/media/OldManInDingyNoBG.83756dd6.png"
+  },
+  {
+    "revision": "bfc4bc331123cf9b9895c88973c87314",
+    "url": "/TomEnglishAI/static/media/React1.bfc4bc33.jpg"
+  },
+  {
+    "revision": "9b36f3d0389a8363f1f91c6b156a400f",
+    "url": "/TomEnglishAI/static/media/ScreenScrape.9b36f3d0.jpg"
+  },
+  {
+    "revision": "498745dfee296aca636d6a046c2920d5",
+    "url": "/TomEnglishAI/static/media/ScreenScrape2.498745df.jpg"
+  },
+  {
+    "revision": "11adf02a8f681655a94223a38e1a98cc",
+    "url": "/TomEnglishAI/static/media/Terms_Geek.11adf02a.jpg"
+  },
+  {
+    "revision": "a47634e1afbb285698eee3ece54b52fa",
+    "url": "/TomEnglishAI/static/media/Terms_Geek2.a47634e1.jpg"
+  },
+  {
+    "revision": "978b9cbc63c6b2d39cd0e9805d7735f6",
+    "url": "/TomEnglishAI/static/media/clip_image002.978b9cbc.jpg"
+  }
+]);
