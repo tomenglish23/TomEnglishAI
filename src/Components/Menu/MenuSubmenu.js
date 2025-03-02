1231f75fe61e87e885b62f2fcd8703aea6c3e44f
tomenglish23 <tomenglish23@gmail.com>
2025-03-02 14:29:25 -0500

    more debugging basename

--- src/Components/Menu/MenuSubmenu.js
+++ src/Components/Menu/MenuSubmenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../CSS/MenuSubmenu.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Path: src\Components\Menu\MenuSubmenu.js
 
@@ -7,50 +8,57 @@ export default function MenuSubmenu() {
  
     /* Need images of each CP */
 
+    const publicUrl = process.env.PUBLIC_URL;
+
   return (
 <div>
     <ul id="menu">
-        <li><a href="\TomEnglishAI\Home"                    >Home</a></li>
+        <li><a href="\TomEnglishAI\TomEnglish\Home"                      >Home</a></li>
 
-        <li><a href="\AIStocks\AIStocksCandlesticks"        >AI Stocks</a> 
+        <li><a href="\TomEnglishAI\AIStocks\AIStocksCandlesticks"        >AI Stocks</a> 
             <ul className="sub">
-                <li><a href="\AIStocks\AIStocksCandlesticks"        >Candlesticks</a></li>
-                <li><a href="\AIStocks\AIStocks5MinCandlesticks"    >5-Min Candlesticks</a></li>
-                <li><a href="\AIStocks\AIStocksPrimitiveAlgorithms" >Primitive Algorithms</a></li>
-                <li><a href="\AIStocks\AIStocksTimeFrames"          >TimeFrames</a></li>
-                <li><a href="\AIStocks\AIStocksMachineLearning"     >Machine Learning</a></li>
+                {/* <li><a href="AIStocksCandlesticks">Candlesticks bad</a></li>
+                <li><a href="AIStocks\AIStocksCandlesticks">Candlesticks bad</a></li>
+                <li><a href="{publicUrl}\AIStocks\AIStocksCandlesticks">Candlesticks bad</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksCandlesticks">Candlesticks bad</a></li> */}   
+
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksCandlesticks">Candlesticks</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocks5MinCandlesticks"    >5-Min Candlesticks</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksPrimitiveAlgorithms" >Primitive Algorithms</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksTimeFrames"          >TimeFrames</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksMachineLearning"     >Machine Learning</a></li>
             </ul>
         </li>
 
-        <li><a href="\AIStocks\AIStocksExternalFactors"      >External</a> 
+        <li><a href="\TomEnglishAI\AIStocks\AIStocksExternalFactors"      >External</a> 
             <ul className="sub">
-                <li><a href="\AIStocks\AIStocksExternalFactors"     >External Factors</a></li>
-                <li><a href="\AIStocks\AIStocksIndexes"             >Indexes</a></li>
-                <li><a href="\AIStocks\AIStocksScreenScrapers"      >Screen Scrapers</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksExternalFactors"     >External Factors</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksIndexes"             >Indexes</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksScreenScrapers"      >Screen Scrapers</a></li>
             </ul>
         </li>
 
-        <li><a href="\AIStocks\AIStocksAPIs"                >Console Apps</a> 
+        <li><a href="\TomEnglishAI\AIStocks\AIStocksAPIs"                >Console Apps</a> 
             <ul className="sub">
-                <li><a href="\AIStocks\AIStocksAPIs"                >Stock APIs</a></li>
+                <li><a href="\TomEnglishAI\AIStocks\AIStocksAPIs"                >Stock APIs</a></li>
             </ul>
         </li>
 
-        <li><a href="\AIArchitecture\AIArchitecture"        >Architecture</a>
+        <li><a href="\TomEnglishAI\AIArchitecture\AIArchitecture"        >Architecture</a>
             <ul className="sub">
-                <li><a href="\AIArchitecture\AIArchitecture"        > Architecture - The Big Picture</a></li>
-                <li><a href="\AIArchitecture\AITechnologies"        > Machine Learning Technologies</a></li>
-                <li><a href="\AIArchitecture\AIHowTo"               > How To - My Quick Reference</a></li>
+                <li><a href="\TomEnglishAI\AIArchitecture\AIArchitecture"        > Architecture - The Big Picture</a></li>
+                <li><a href="\TomEnglishAI\AIArchitecture\AITechnologies"        > Machine Learning Technologies</a></li>
+                <li><a href="\TomEnglishAI\AIArchitecture\AIHowTo"               > How To - My Quick Reference</a></li>
             </ul>
         </li>
 
-        <li><a href="\TomEnglishAI\About"                   >About</a>
+        <li><a href="\TomEnglishAI\TomEnglish\About"                   >About</a>
         <ul className="sub">
-                <li><a href="\TomEnglish\About"                     > About</a></li>
-                <li><a href="\TomEnglish\TomEnglishAIResume"        > TomEnglish AI Resume</a></li>
-                <li><a href="\TomEnglish\HowAIEvolved"              > How AI evolved for me</a></li>
-                <li><a href="\TomEnglish\BusinessPlan"              > Business Plan</a></li>
-                <li><a href="\TomEnglish\AWSDictionary"             > AWS Dictionary</a></li>
+                <li><a href="\TomEnglishAI\TomEnglish\About"                     > About</a></li>
+                <li><a href="\TomEnglishAI\TomEnglish\TomEnglishAIResume"        > TomEnglish AI Resume</a></li>
+                <li><a href="\TomEnglishAI\TomEnglish\HowAIEvolved"              > How AI evolved for me</a></li>
+                <li><a href="\TomEnglishAI\TomEnglish\BusinessPlan"              > Business Plan</a></li>
+                <li><a href="\TomEnglishAI\TomEnglish\AWSDictionary"             > AWS Dictionary</a></li>
             </ul>
         </li>
 
